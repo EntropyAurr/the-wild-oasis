31f813d15ffa8ca0ad2de6138ede0c5398c9ab8b
Au Cao Thuy Duong <auduong.301@gmail.com>
2025-05-24 12:52:47 +0700

    update Modal

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,10 +1,33 @@
-import { useState } from "react";
-
 import Button from "../../ui/Button";
-import Modal from "../../ui/Modal";
+import Modal from "../../ui/Modal-v1";
+import CabinTable from "./CabinTable-v1";
 import CreateCabinForm from "./CreateCabinForm-v1";
 
 function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button variation="primary" size="medium">
+          Add new Cabin
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button variation="primary" size="medium">
+          Show table
+        </Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
+}
+
+/* function AddCabin() {
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
@@ -21,5 +44,5 @@ function AddCabin() {
     </div>
   );
 }
-
+ */
 export default AddCabin;
