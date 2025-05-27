232dd3cf9b2f519d27d1a292c97e68513443de4f
Au Cao Thuy Duong <auduong.301@gmail.com>
2025-05-27 08:31:50 +0700

    update

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -2,11 +2,7 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 export async function getBooking(id) {
-  const { data, error } = await supabase
-    .from("bookings")
-    .select("*, cabins(*), guests(*)")
-    .eq("id", id)
-    .single();
+  const { data, error } = await supabase.from("bookings").select("*, cabins(*), guests(*)").eq("id", id).single();
 
   if (error) {
     console.error(error);
@@ -51,13 +47,7 @@ export async function getStaysAfterDate(date) {
 
 // Activity means that there is a check in or a check out today
 export async function getStaysTodayActivity() {
-  const { data, error } = await supabase
-    .from("bookings")
-    .select("*, guests(fullName, nationality, countryFlag)")
-    .or(
-      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
-    )
-    .order("created_at");
+  const { data, error } = await supabase.from("bookings").select("*, guests(fullName, nationality, countryFlag)").or(`and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`).order("created_at");
 
   // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
   // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
@@ -71,12 +61,7 @@ export async function getStaysTodayActivity() {
 }
 
 export async function updateBooking(id, obj) {
-  const { data, error } = await supabase
-    .from("bookings")
-    .update(obj)
-    .eq("id", id)
-    .select()
-    .single();
+  const { data, error } = await supabase.from("bookings").update(obj).eq("id", id).select().single();
 
   if (error) {
     console.error(error);
